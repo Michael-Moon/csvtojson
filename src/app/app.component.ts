512fccea85d592f3d5d43c455079ce17e0004819
Michael <44618004+Michael-Moon@users.noreply.github.com>
2022-01-13 12:18:34 -0300

    converte arquivo excel para json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csvtojson';
+  convertedJson!: string;
+
+  fileUpload(event: any ){
+
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (e: any) => {
+
+      let binaryData = e.target?.result;
+      let workbook = XLSX.read(binaryData, {type: 'binary'});
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+
+        this.convertedJson = JSON.stringify(data, undefined, 4);
+        console.log(this.convertedJson);
+      })
+
+    }
+  }
 }
